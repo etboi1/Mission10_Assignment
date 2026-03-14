b8f544addb6bb523ee66dc847b2ce255865418bd
etboi1 <et.grundvig@gmail.com>
2026-03-13 20:52:13 -0600

    Add project files.

---
+++ Mission10_Assignment/Controllers/BowlingLeagueController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mission10_Assignment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BowlingLeagueController : ControllerBase
+    {
+        private FoodDbContext _foodContext;
+        public MarriottFoodController(FoodDbContext temp)
+        {
+            _foodContext = temp;
+        }
+
+        [HttpGet(Name = “GetMArriottFood”)]
+        public IEnumerable<MarriottFood> Get() {
+            IEnumerable<MarriotFood> foodList = _foodContext.Foods.ToList();
+            return (foodList);
+        }
+
+    }
+}
---
+++ Mission10_Assignment/Data/Bowler.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mission10_Assignment.Data
+{
+    public class Bowler
+    {
+        [Key]
+        public int BowlerID { get; set; }
+        public string BowlerLastName { get; set; }
+        public string BowlerFirstName { get; set; }
+        public char BowlerMiddleInit { get; set; }
+        public string BowlerAddress { get; set; }
+        public string BowlerCity { get; set; }
+        public string BowlerState { get; set; }
+        public string BowlerZip { get; set; }
+        public string BowlerPhoneNumber { get; set; }
+        public int TeamID { get; set; }
+        [ForeignKey("TeamID")]
+        public Team Team { get; set; }
+    }
+}
